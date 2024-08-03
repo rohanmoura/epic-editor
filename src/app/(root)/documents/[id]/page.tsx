dcc36ac1d838190c7956d9728e6933fda78b14f7
rohanmoura <rsmoura29@gmail.com>
2024-08-03 11:39:36 +0530

    Authentication - Clerk

--- src/app/(root)/documents/[id]/page.tsx
+++ src/app/(root)/documents/[id]/page.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
@@ -7,9 +8,15 @@ const page = () => {
       <Header>
         <div className='flex w-fit items-center justify-center gap -2'>
           <p className='document-title'>
-            This is a fake document title
+            Share
           </p>
         </div>
+        <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
       </Header>
     </div>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,13 +24,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <ClerkProvider appearance={{
+      baseTheme: dark,
+      variables: {
+        colorPrimary: "#3371ff",
+        fontSize: "16px",
+      },
+    }}>
+      <html lang="en" suppressHydrationWarning>
         <body
           className={cn(
             "min-h-screen font-sans antialiased",
             inter.variable
           )}
         >{children}</body>
-    </html>
+      </html>
+    </ClerkProvider>
   );
 }
