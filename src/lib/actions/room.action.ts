2ef8462c037f56ff333db6d4f7f1efd4e0c7fa88
rohanmoura <rsmoura29@gmail.com>
2024-08-03 17:33:26 +0530

    list all documents on home page

--- src/lib/actions/room.action.ts
+++ src/lib/actions/room.action.ts
@@ -65,3 +65,13 @@ export const updateDocument = async (roomId: string, title: string) => {
         console.log("Error happened while updating the document");
     }
 }
+
+export const getDocuments = async (email: string) => {
+    try {
+        const rooms = await liveblocks.getRooms({ userId: email });
+
+        return parseStringify(rooms)
+    } catch (error) {
+        console.log("Error happened while getting the documents");
+    }
+}
